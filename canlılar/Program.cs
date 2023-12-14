8fb53f4709f02dcd3306ddcc6be1f218d556ad5e
samed652 <samed652@BPC05>
2023-12-14 15:13:20 +0300

    Proje dosyası ekle.

---
+++ canlılar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace canlılar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Canli
+    {
+        public string beslenme { get; set; }
+        public string nefes { get; set; }
+    }
+    public class Hayvan : Canli
+    {
+        public string hareket{ get; set; }
+    }
+    public class Kopek : Hayvan
+    {
+        public string havlamak { get; set; }
+    }
+    public class Kangal : Kopek
+    {
+        public string guclu { get; set; }
+    }
+}
